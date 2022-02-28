2c3806c7839d7e8e50b2b0343e56c3daa891e065
Andrea Botello <andrea.botellor@misena.edu.co>
2022-02-28 18:56:42 -0500

    updated

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -3,7 +3,151 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
+    .titulo-principal.color-secundario
+      .titulo-principal__numero
+        span 3
+      h1 <em>Software</em> de simulación de circuitos electrónicos digitales
 
+    .row.justify-content-center.mb-5(data-aos="fade-down")
+      .col-md-10.tarjeta.bg-degrade-verde.borde-redondo
+        .row.justify-content-center
+          .col-md-10.py-3.text-white
+            p.mb-0 Los <em>software</em> de simulación son una herramienta muy útil y de amplia aplicación en la implementación de circuitos electrónicos digitales, estas permiten concebir los diseños desde la etapa inicial, permitiendo incluso, hacer algunos montajes sin los cálculos previos, es decir, aplicando solo la lógica, algunos de ellos son los siguientes.
+
+    .row.justify-content-center
+      .col-md-10
+        PasosA.color-acento-contenido(tipo="n")
+          .row.align-items-center
+            .col-md-3.mb-4.mb-md-0
+              figure
+                img(src='@/assets/curso/tema3-paso1.jpg', alt='Proteus')
+            .col-md-9
+              p Uno de los <em>software</em> más utilizados es Proteus. Sin embargo, es una herramienta con licencia cerrada y se debe pagar para su uso; además de la simulación este permite el diseño de la PCB. 
+          .row.align-items-center
+            .col-md-9.mb-4.mb-md-0
+              p Otra herramienta potente para la simulación y desarrollo es el Altium, también de licencia propietaria de grandes prestaciones.
+            .col-md-3
+              figure
+                img(src='@/assets/curso/tema3-paso2.jpg', alt='Altium')
+          .row.align-items-center
+            .col-md-3.mb-4.mb-md-0
+              figure
+                img(src='@/assets/curso/tema3-paso3.jpg', alt='Aplicaciones')
+            .col-md-9
+              p Existen muchas aplicaciones gratuitas y de funcionamiento online, que permiten la simulación efectiva de la mayoría de los circuitos digitales, algunas de ellas parten del diagrama esquemático y muestran sobre este los estados lógicos, es decir, en los mismos diagramas de las compuertas lógicas, se muestran los resultados.
+          .row.align-items-center
+            .col-md-9.mb-4.mb-md-0
+              p Algunos programas con gráficos 3D permiten visualizar el mismo circuito integrado físico, montado sobre la protoboard, tratando de representarlo, como se vería en la realidad. Estas versiones las hay incluso de uso libre y en línea, Tinkercad es una de ellas.
+            .col-md-3
+              figure
+                img(src='@/assets/curso/tema3-paso4.jpg', alt='gráficos 3D')
+   
+    Separador
+
+
+    #t_3_1
+    .titulo-segundo.color-acento-contenido
+      h2 3.1. Tipos y aplicación
+
+    p.mb-5(data-aos="fade") Dentro de los tipos de simuladores encontramos diferentes características entre ellos, siendo la más sobresaliente la relacionada con el tipo de simulación y la interfaz mostrada al usuario, se encuentran, por ejemplo:
+
+    TabsA.color-acento-contenido.mb-5
+      .tarjeta.bg-celeste3.p-4(titulo="Simulador esquemático")
+        .row.align-items-center
+          .col-md-6
+            h4 Simulador esquemático
+            p Este tipo de simulador permite dibujar el diagrama esquemático y realizar la simulación sobre los símbolos de los mismos elementos. Son los más utilizados por personal avanzado, ya que permite interpretar funcionamiento de manera más precisa, pero se requiere de mayor conocimiento de los procedimientos. 
+            p Proteus es un ejemplo de este tipo de <em>software</em>, la imagen muestra los estados en la salida de los elementos. Los recuadros en cada una de las entradas y salidas indican un 0 lógico para el color azul y un 1 lógico para el color rojo. 
+            p Circuit Maker, y <em>software</em> en línea como Circuitlab, también son ejemplos de simuladores esquemáticos
+          .col-md-6
+            img(src='@/assets/curso/tema3-1-tab1.jpg', alt='Simulador esquemático')
+      .tarjeta.bg-celeste3.p-4(titulo="Simulador con ilustraciones realistas")
+        .row.align-items-center
+          .col-md-6
+            h4 Simulador con ilustraciones realistas
+            p Este tipo de <em>software</em> permite visualizar el componente como si se tratara de su presentación física. Se puede ver una protoboard o incluso la PCB; Tinkercad o Multisim son ejemplos de este tipo de <em>software</em>. 
+            p Es tipo de programas son recomendados para principiantes y para personas que quieren adquirir destreza en el manejo y la identificación de componentes.
+          .col-md-6
+            img(src='@/assets/curso/tema3-1-tab2.jpg', alt='Simulador con ilustraciones realistas')   
+            p.mb-0 Simulación realista. Captura de pantalla de ThinkerCad. 
+      .tarjeta.bg-celeste3.p-4(titulo="Simulador de plataforma")
+        .row.align-items-center
+          .col-md-6
+            h4 Simulador de plataforma
+            p Este tipo de <em>software</em> de simulación casi siempre se apoya de una plataforma o tarjeta integrada, donde el circuito pasa a ser, de una simulación a un prototipo real. 
+            p Arduino es un ejemplo de este tipo de plataformas. También encontramos a Coolrunner y Basys.
+          .col-md-6
+            img(src='@/assets/curso/tema3-1-tab3.jpg', alt='Simulador de plataforma')  
+
+
+    p.mb-5(data-aos="fade") Para efectos de aplicación se utilizarán sólo dos tipos de simulador: los esquemáticos y los de gráficos realistas. A continuación se explica cada uno de ellos. 
+
+    ImagenInfograficaB.color-acento-contenido.tema3-1.pb-5
+      template(v-slot:imagen)
+        figure
+          img(src='@/assets/curso/tema3-1-infog.png', alt='dos tipos de simulador')
+
+      div.tarjeta.p-3(x="37%" y="56%" tooltip="Simulador esquemático")
+        .h3.mb-2 Simulador esquemático
+        p De estos encontramos <em>software</em> en línea de tipo gratuito. Un ejemplo es CircuitLab, en el cual se puede iniciar con las compuertas lógicas para implementar un circuito como el de la imagen y comprobar luego en la tabla de verdad los resultados. Para usar este <em>software</em> en línea debe crear una cuenta.
+        .row.justify-content-center
+          .col-md-9.mb-4
+            img(src='@/assets/curso/tema3-1-imginfog1.jpg', alt='Simulador esquemático')
+        p También puede probar la equivalencia de circuitos; por ejemplo, se puede aplicar la ley distributiva armando en paralelo los circuitos correspondientes y comprobar que la salida es la misma para todos los valores de la tabla de verdad.
+
+      div.tarjeta.p-3(x="63%" y="56%" tooltip="Simulador gráfico realista")
+        .h3.mb-2 Simulador gráfico realista
+        p Para simular un circuito con interfaz realista se utiliza la herramienta gratuita Tinkercad. Para el manejo de las compuertas lógicas se debe tener claro sus preferencias y diagramas internos, según la hoja de datos; estas son la referencias más comunes:
+        .row.justify-content-center
+          .col-md-4.mb-4
+            ul.lista-ul.px-md-4.px-2
+              li.mb-0
+                i.lista-ul__vineta
+                | AND = 7408.
+              li.mb-0 
+                i.lista-ul__vineta
+                | OR = 7432.
+              li.mb-0 
+                i.lista-ul__vineta
+                | NOT = 7404.
+          .col-md-4.mb-4
+            ul.lista-ul.px-md-4.px-2
+              li.mb-0
+                i.lista-ul__vineta
+                | NAND =7400.
+              li.mb-0 
+                i.lista-ul__vineta
+                | NOR = 7402.
+              li.mb-0 
+                i.lista-ul__vineta
+                | XOR = 7486.
+        p.mb-4 Es necesario hacer uso de los conocimientos de electrónica para interpretar el montaje en la protoboard. Una de las ventajas de los <em>software</em> de simulación con ilustraciones realistas, tal como lo presenta la imagen, es que permiten ver el montaje como sería en la realidad y luego al armarlo de forma física, la probabilidad de cometer errores en el montaje, es menor.
+        .row.justify-content-center
+          .col-md-9
+            img(src='@/assets/curso/tema3-1-imginfog2.jpg', alt='Simulador esquemático')
+
+    Separador
+
+
+    #t_3_2
+    .titulo-segundo.color-acento-contenido
+      h2 3.2. Generación de archivos de trabajo
+
+    p(data-aos="fade") Los archivos de trabajo en la implementación de circuitos electrónicos digitales está limitada a la importación de las imágenes y los listados de componentes. Para el desarrollo y el prototipado es probable que se necesiten generar archivos de fresado, serigrafía y otros.
+
+    p(data-aos="fade") Para el caso de Tinkercad, por ejemplo, es solo dar clic sobre el ícono de listado, se pone en modo lista y luego se importa el archivo en dicho formato, tal como se presenta a continuaciónn.
+
+    .row.justify-content-center.mb-5
+      .col-md-10
+        .row.borde-botones.justify-content-center
+          img.py-3(src='@/assets/curso/tema3-2-img.jpg', alt='Tinkercad en modo List') 
+          h5.text-white.bg-botones1.py-2.mb-0 Nota. Tinkercad en modo List. Captura de pantalla.  
+
+    .row.justify-content-center.mb-5(data-aos="fade-down")
+      .col-md-10.tarjeta.bg-primario1
+        .row.justify-content-center
+          .col-md-10.py-3.text-white
+            p.mb-0 El archivo esquemático se puede importar en varios formatos incluyendo el formato de imagen .jpg o .png. Para el caso de los circuitos digitales solo será necesario importar un archivo de imagen para guardar los esquemas utilizados en la implementación.
 
 </template>
 
--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -3,7 +3,176 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
+    .titulo-principal.color-secundario
+      .titulo-principal__numero
+        span 4
+      h1 Ensamble de circuitos digitales
 
+    p(data-aos="fade") Anteriormente el ensamble de circuitos digitales era un proceso de cuidado, con el avance de la tecnología y la implementación de las tarjetas integradas, este proceso se ha vuelto más flexible y más confiable. 
+
+    p.mb-5(data-aos="fade") Los circuitos digitales generalmente requieren de tensiones reguladas para su operación, si esto no es así, se vuelven inestables y resultan en un funcionamiento erróneo y poco confiable, más si no se respetan las reglas de diseño.
+
+    .row.justify-content-center
+      .col-md-10(data-aos="fade-down")
+        .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/tema4-img.jpg')})`}")
+          .bloque-texto-g__texto.p-4
+            p.mb-0 El grosor de pistas, mal trazo, capacitancias parásitas, cables muy largos son algunos de los factores que influyen en el mal funcionamiento de estos circuitos.
+
+    Separador
+
+
+    #t_4_1
+    .titulo-segundo.color-acento-contenido
+      h2 4.1. Prototipos
+
+    p.mb-5(data-aos="fade") En todo diseño de circuitos electrónicos es necesario pasar por el proceso de prototipado, este nos permite probar funcionalidad, hacer ajustes y hacer algunos cambios, aunque el prototipo no pasa a ser el diseño final, es necesario aplicar todos los procesos ideados, ya que el objetivo de este, es demostrar que es completamente funcional. La siguiente figura 7 presenta el sentido de su desarrollo.
+
+    .row.justify-content-center.mb-0
+      .col-md-10
+        .row.borde-botones
+          h4.text-center.text-white.bg-botones1.py-1.mb-4 Importancia del prototipado
+          .row.justify-content-center
+            .col-md-10.tarjeta.bg-gris4.py-3.px-4.mb-2
+              p.mb-0 <b>Lo primero que debemos tener claro</b> para iniciar el proceso de prototipado, es precisamente el sentido de este. Un prototipo es un montaje de prueba, eso es lo que indica el nombre:  
+            .col-md-12.py-4
+              .row.align-items-end
+                .col-md-7.z-999
+                  img(src='@/assets/curso/tema4-1-img.jpg', alt='Imagen decorativa') 
+                .col-md-5
+                  .row.align-items-center
+                    .col-md-5
+                      p.mb-0 <b style="font-size: 18px;"> Proto</b> (prueba)
+                    .col-md-2.p-0
+                      h2.mb-0(style="color: #43BABE; font-size: 80px;") +  
+                    .col-md-5
+                      p <b style="font-size: 18px;">Tipo</b> (Modelo que reúne los caracteres esenciales de un conjunto y que sirve como pauta para imitarlo, reproducirlo o copiarlo).
+                  p Al referirnos al prototipo de un circuito electrónico, esto nos quiere decir que al armarlo debe cumplir con todas las especificaciones del circuito final. 
+    .row.justify-content-center.mb-5
+      .col-md-10.bg-gris.borde-botones-sin-top
+        .row.justify-content-center
+          .col-md-10.py-3
+            p.text-center.mb-0 Sirve para corregir errores y hacer ajustes de funcionamiento.
+
+    .row.justify-content-center.mb-4
+      .col-md-9(data-aos="fade-down")
+        .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/tema4-1-img2.jpg')})`}")
+          .bloque-texto-g__texto.p-4
+            p.mb-0 Una de las herramientas con la que se debe contar para iniciar el proceso de prototipado es la protoboard, la cual permite realizar montajes previos y ser probados sin destruir elementos, antes de llegar al diseño final.
+
+    p(data-aos="fade") Para el caso de los circuitos digitales con compuertas, el prototipo se limita a la protoboard, de aquí parte directamente a la implementación en PCB.
+
+    Separador
+
+
+    #t_4_2
+    .titulo-segundo.color-acento-contenido
+      h2 4.2. Conexionado
+
+    p(data-aos="fade") El conexionado influye en el funcionamiento del circuito, sobre todo si no se tiene en cuenta el apantallamiento y eliminación de ruido eléctrico, es por eso que hay que centrarse en los parámetros generales del circuito; es decir aquello que no varía mucho; tal es el caso de los componentes electrónicos (resistores, capacitores, bobinas, circuitos integrados, cables) y la placa de circuito impreso.
+
+    p.mb-4(data-aos="fade") Es importante definir bien los parámetros para estos componentes, cualquier error, por mínimo que parezca, puede desencadenar en un mal funcionamiento del circuito. Naturalmente estos procesos han evolucionado. A través del tiempo se han dado las pautas para ir estableciendo reglas de diseño, todas ellas encaminadas al mejor desempeño, a la menor afectación posible. La siguiente figura 8 detalla un poco más sobre estos parámetros.
+
+    .row.justify-content-center.mb-5(data-aos="fade-down")
+      .col-md-10.tarjeta.bg-botones1  
+        .row.justify-content-center
+          .col-md-10.py-3.text-white
+            p.mb-0 Las reglas de diseño en sí son varios parámetros que establecen el conexionado,  aunque muchas de estas se definen en el <em>software</em> de simulación y modelado; también, hay que tener en cuenta algunos aspectos externos. 
+
+    .row.justify-content-center(data-aos="fade-down")
+      .col-md-9
+        ImagenInfografica.color-acento-contenido.tema4-2
+          template(v-slot:imagen)
+            figure
+              img(src='@/assets/curso/tema4-2-img.svg', alt='Conexionado')
+
+          .tarjeta.bg-gris4.p-3(x="5%" y="81%" numero="+")
+            .h4.text-center.mb-0 Cantidad de capas
+          .tarjeta.bg-gris4.p-3(x="7%" y="57%" numero="+")
+            .h4.text-center.mb-0 Grosor de pistas en la placa
+          .tarjeta.bg-gris4.p-3(x="13%" y="30%" numero="+")
+            .h4.text-center.mb-0 Separación de pistas
+          .tarjeta.bg-gris4.p-3(x="25%" y="12%" numero="+")
+            .h4.text-center.mb-0 Colocación de componentes
+          .tarjeta.bg-gris4.p-3(x="49%" y="6%" numero="+")
+            .h4.text-center.mb-0 Pad
+          .tarjeta.bg-gris4.p-3(x="75%" y="12%" numero="+")
+            .h4.text-center.mb-0 Grilla
+          .tarjeta.bg-gris4.p-3(x="88%" y="30%" numero="+")
+            .h4.text-center.mb-0 Desacoples
+          .tarjeta.bg-gris4.p-3(x="92%" y="57%" numero="+")
+            .h4.text-center.mb-0 Chasis
+          .tarjeta.bg-gris4.p-3(x="95%" y="81%" numero="+")
+            .h4.text-center.mb-0 Fijación de conectores  
+    
+    Separador
+
+
+    #t_4_3
+    .titulo-segundo.color-acento-contenido
+      h2 4.3. Tarjetas integradas
+
+    p(data-aos="fade") Las tarjetas integradas se constituyeron en una revolución para la implementación de circuitos electrónicos digitales. Son las responsables de prácticamente la desaparición de los circuitos integrados de compuertas digitales, ya que pueden desarrollar cualquier circuito sin necesidad de montajes. En algunos casos solo es necesario implementar ciertos componentes periféricos y naturalmente en conexionado de componentes complementarios. Son ejemplos de estas tarjetas integradas: Basys con la interfaz Xilinx,  Basic Stam, Energía, Elvis de Nationals Instruments, y los que usan tarjetas embebidas como Coolrunner II. 
+
+    p(data-aos="fade") Este tipo de plataforma, permiten la implementación de todo un circuito digital sin usar compuertas lógicas externas. Todas son creadas en un chip conocido como Arreglo Lógico Programable (PAL, por sus siglas en inglés). Las plataformas más avanzadas son conocidas como FPGA (Field Programmable Gate Arrays)
+
+    p.mb-4(data-aos="fade") La siguiente imagen presenta un ejemplo de una tarjeta integrada Basys 3 FPGA con sus componentes.
+
+    .row.justify-content-center.mb-0
+      .col-md-10
+        .row.borde-botones
+          .row.justify-content-center
+            .col-md-9
+              img(src='@/assets/curso/tema4-3-img.svg', alt='Imagen decorativa') 
+          h5.text-white.bg-botones1.py-2.mb-0 Nota. Tomada de Mouser Electronics (2020)        
+
+    Separador
+
+
+    #t_4_4
+    .titulo-segundo.color-acento-contenido
+      h2 4.4. <em>Software</em> de aplicación
+
+    p Generalmente el <em>software</em> utilizado para simular el circuito es el mismo usado para su implementación final; en el caso de no hacerlo puede generar el archivo para implementarlo en aplicaciones que sí lo hagan. En el caso de las tarjetas integradas solo es necesario transferir el código a la tarjeta, normalmente esto lo hace la misma aplicación de desarrollo y solo es necesario conectarla al PC usando un puerto USB. 
+
+    .row.justify-content-center.mb-0
+      .col-md-10
+        .row.borde-botones
+          h4.text-center.text-white.bg-botones1.py-1.mb-4 Tarjeta Arduino (Kit de componentes)
+          .row.justify-content-center
+            .col-md-10.mb-4
+              img(src='@/assets/curso/tema4-4-img.jpg', alt='Tarjeta Arduino (Kit de componentes)') 
+      .col-md-10.bg-gris.borde-botones-sin-top.mb-5
+        .row.justify-content-center
+          .col-md-10.py-3
+            p.mb-0 Una de las tarjetas de desarrollo más utilizada en la actualidad es Arduino, la cual permite la programación e implementación desde cualquier circuito lógico simple, hasta un circuito complejo.
+
+    .row.justify-content-center.mb-5
+      .col-md-8(data-aos="fade-down")
+        .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/tema4-4-img2.jpg')})`}")
+          .bloque-texto-g__texto.p-4
+            p.mb-0 El <em>software</em> utilizado para programar este tipo de plataformas, es sencillo y de fácil implementación algunos de ellos son de tipo gráfico como microbit (https://microbit.org/) pensado para robótica en niños. 
+    
+    .row.justify-content-center(data-aos="fade-down")
+      .col-md-10
+        .tarjeta.bg-degrade-morado.p-3
+          .row.justify-content-around.align-items-center
+            .col-3.col-sm-2.col-lg-2.px-lg-4.imagen_girando
+              img.px-lg-3.mb-0(src="@/assets/curso/banner2.png")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0.text-white
+                  h4.mb-2 Arduino
+                  p.text-small.mb-0 El siguiente video es un tutorial de arduino que complementa la información, este también lo podrá encontrar en el material de apoyo.
+                .col-sm-auto
+                  a.boton.color-acento-botones.text-white(href="https://www.youtube.com/watch?v=eBVvD85Ml2c&list=PLkjnQ3NFTPnY1eNyLDGi547gkVui1vyn2" target="_blank")
+                    span Video
+                    i.fas.fa-play-circle
 
 </template>
 
