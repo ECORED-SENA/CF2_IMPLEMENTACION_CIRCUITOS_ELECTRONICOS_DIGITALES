a028603b859545e42c8ac6ecc174ff706bc791e6
Andrea Botello <andrea.botellor@misena.edu.co>
2022-03-03 10:45:51 -0500

    updated

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -10,17 +10,34 @@
             img.px-md-4(src="@/assets/curso/act-didactica.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Análisis de circuitos digitales
+          h2 Análisis de circuitos digitales 1
         p.mb-4 Afianzar los conocimientos adquiridos durante el desarrollo de la temática.
         .tarjeta.tarjeta--azul.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Arrastrar y soltar.
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/act1/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
-
+    
+    .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
+      .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
+        .tarjeta.tarjeta--azul.h-100.d-flex.align-items-center
+          figure
+            img.px-md-4(src="@/assets/curso/act-didactica.svg", alt="Texto que describa la imagen")
+      .col-12.col-md-8.col-lg-9
+        .titulo-segundo
+          h2 Análisis de circuitos digitales 2
+        p.mb-4 Afianzar los conocimientos adquiridos durante el desarrollo de la temática.
+        .tarjeta.tarjeta--azul.p-3
+          .row.justify-content-around.align-items-center            
+            .col-sm.mb-3.mb-sm-0
+              p.fw-bold.mb-0 Arrastrar y soltar.
+            .col-auto
+              a.boton.boton--b(:href="obtenerLink('/act2/story.html')" target="_blank")
+                span Realizar
+                i.fas.fa-puzzle-piece
     
 
     
